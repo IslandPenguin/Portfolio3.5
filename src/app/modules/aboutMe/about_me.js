d7de56ce41a0112d2d9ff435ea146902d3c0f85a
Danijel Demirovic <cucak92@gmail.com>
2021-04-10 20:17:03 +0200

    Update about_me.js

--- src/app/modules/aboutMe/about_me.js
+++ src/app/modules/aboutMe/about_me.js
@@ -8,7 +8,7 @@ import { random, parseNewLine } from "Common/utils";
 import { getImagePosition, getBackgroundTransition } from './about_me_helper';
 import avatarImg from "Images/background/Aut.OptProject.png";
 import {Fade} from "react-awesome-reveal";
-import Signature from "Images/signature.png";
+import Signature from "Images/signature-croped.png";
 
 class AboutMe extends Component {
   constructor(props) {
